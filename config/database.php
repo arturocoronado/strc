55de683bd225c92e0f81edbf9fae38c0f40eee31
mitnickgt <mitnickgt@gmail.com>
2018-01-17 12:10:40 -0600

    hola x

--- config/database.php
+++ config/database.php
@@ -58,7 +58,7 @@ return [
             'driver' => 'pgsql',
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'declaranet'),
+            'database' => env('DB_DATABASE', 'declaranetX'),
             'username' => env('DB_USERNAME', 'postgres'),
             'password' => env('DB_PASSWORD', 'postgres'),
             'charset' => 'utf8',
