e6f53d4dfb288a3af538b04e614f0eb28a898c9e
jorgemacias <oyejorge@hotmail.com>
2018-01-22 16:02:11 -0600

    Parametros

--- app/Http/Controllers/ParametrosController.php
+++ app/Http/Controllers/ParametrosController.php
@@ -14,13 +14,20 @@ class ParametrosController extends Controller {
      */
     public function index() {
         $params[] = array("Header" => "#", "Width" => "40", "Attach" => "", "Align" => "center", "Sort" => "na", "Type" => "ro");
-        $params[] = array("Header" => "Opción", "Width" => "*", "Attach" => "", "Align" => "center", "Sort" => "na", "Type" => "ro");
-        $params[] = array("Header" => "Valor", "Width" => "*", "Attach" => "", "Align" => "center", "Sort" => "na", "Type" => "ed");
+        $params[] = array("Header" => "Opción", "Width" => "*", "Attach" => "", "Align" => "left", "Sort" => "na", "Type" => "ro");
+        $params[] = array("Header" => "Valor", "Width" => "*", "Attach" => "", "Align" => "center", "Sort" => "na", "Type" => "ro");
         $params[] = array("Header" => "Descripción", "Width" => "*", "Attach" => "txt", "Align" => "left", "Sort" => "na", "Type" => "ro");
 
 
         return view('config.parametros_index')
                         ->with('params', $params);
+    }
+
+    public function edit(Request $r) {
+        $parametros = Parametro::orderBy('Orden', 'asc')->get();
+
+        return view('config.parametros_form')
+                        ->with('parametros', $parametros);
     }
 
     /**
@@ -58,10 +65,28 @@ class ParametrosController extends Controller {
      * @return \Illuminate\Http\Response
      */
     public function save(Request $r) {
-        Parametro::where('id', $r->id)->update(array('Valor' => $r->valor));
-        
-        $parametro=Parametro::find($r->id);
-        return response()->json($parametro);
+        $parametros = Parametro::all();
+
+
+        foreach ($parametros as $i => $p) {
+            $id = $p->id;
+            if ($p->Tipo != 'jpg,png,gif' && $p->Tipo != 'pdf') {
+                Parametro::where('id', $id)->update(array('Valor' => $r->$id));
+            } else {
+                $nombre_archivo = 'f' . $id;
+                
+                if ($r->hasFile($nombre_archivo)) {
+                    $imageName = $nombre_archivo . '.' .$r->file($nombre_archivo)->getClientOriginalExtension();
+                    $r->file($nombre_archivo)->store($imageName);
+                } else {
+                    echo 'no hay archivo '.$nombre_archivo;
+                }
+
+                Parametro::where('id', $id)->update(array('Valor' => $imageName));
+            }
+        }
+        $parametros_nuevos = Parametro::all();
+        return response()->json($parametros_nuevos);
     }
 
 }
