5689a1f5f34a87cc60010485396ea57b98e2a1b6
Christian Baltazar <cbaltazarc@guanajuato.gob.mxbrew>
2018-01-24 13:24:38 -0600

    roles

--- database/migrations/2018_01_24_125354_alter_roles.php
+++ database/migrations/2018_01_24_125354_alter_roles.php
@@ -32,7 +32,7 @@ class AlterRoles extends Migration
                 array(
                     'id'   => 1,
                     'Rol'     => 'Master', 
-                    'Permisos' => '1,31,4,5,6,7,8,9,10,11,12,13,14,15,16,17'
+                    'Permisos' => '1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17'
                 )
         );
     }
