a446fc866a390ad2cd80c35fc945c5e2a6764b79
mitnickgt <mitnickgt@gmail.com>
2018-01-26 08:56:19 -0600

    actualizaciones contrataciones

--- app/Contratacion.php
+++ app/Contratacion.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Wildside\Userstamps\Userstamps;
 
-class Contrato extends Model
+class Contratacion extends Model
 {
     use SoftDeletes;
     use Userstamps;
--- routes/web.php
+++ routes/web.php
@@ -52,11 +52,11 @@ Route::group(['prefix' => 'catalogos'], function() {
     Route::post('fracciones/save/{fraccion?}', 'FraccionesController@save')->name('catalogos.fracciones.save');
     Route::get('fracciones/delete/{fraccion}', 'FraccionesController@delete')->name('catalogos.fracciones.delete');
 
-    Route::get('contratos', 'ContratosController@index')->name('catalogos.contratos.index');
-    Route::get('contratos/data', 'ContratosController@data')->name('catalogos.contratos.data');
-    Route::get('contratos/form/{fraccion?}', 'ContratosController@form')->name('catalogos.contratos.form');
-    Route::post('contratos/save/{fraccion?}', 'ContratosController@save')->name('catalogos.contratos.save');
-    Route::get('contratos/delete/{fraccion}', 'ContratosController@delete')->name('catalogos.contratos.delete');
+    Route::get('contrataciones', 'ContratacionesController@index')->name('catalogos.contrataciones.index');
+    Route::get('contrataciones/data', 'ContratacionesController@data')->name('catalogos.contrataciones.data');
+    Route::get('contrataciones/form/{fraccion?}', 'ContratacionesController@form')->name('catalogos.contrataciones.form');
+    Route::post('contrataciones/save/{fraccion?}', 'ContratacionesController@save')->name('catalogos.contrataciones.save');
+    Route::get('contrataciones/delete/{fraccion}', 'ContratacionesController@delete')->name('catalogos.contrataciones.delete');
 });
 
 Route::group(['prefix' => 'padron'], function() {
